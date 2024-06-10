b46330dd38d58069a5a6f0d7d43807f7f0c16dcd
Surabhi Mitra <39255747+Surabhi1997mitra@users.noreply.github.com>
2024-06-10 23:22:03 +0530

    edit button added

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,7 @@
 // This component will display individual to-do items.
 
 import React from 'react';
-import { ListItem, ListItemText, IconButton } from '@material-ui/core';
+import { ListItem, ListItemText, IconButton, TextField } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 
@@ -11,17 +11,26 @@ interface TodoItemProps {
         color: string
     },
     index: number,
-    removeTodo: (index: number) => void;
+    removeTodo: (index: number) => void,
+    editTodo: (index: number, updatedTodo: string) => void,
+    editIndex: number | any,
+    setEditIndex: (index: number) => void;
 }
 
-const TodoItem: React.FC<TodoItemProps> = ({ todo, index, removeTodo }) => {
+const TodoItem: React.FC<TodoItemProps> = ({ todo, index, editTodo, removeTodo, editIndex, setEditIndex }) => {
     return (
         <ListItem style={{ borderRadius: 10, margin: "1rem 0", border: "1px solid grey", padding: 0 }}>
             <div style={{ width: 30, height: 50, marginRight: 10, borderRadius: "10px 0 0 10px", background: todo.color }}></div>
-            <ListItemText primary={todo.name} />
-            {/* <IconButton edge="end" onClick={() => removeTodo(index)} style={{ marginRight: 5 }}>
+            {editIndex === index ?
+            <TextField
+                fullWidth
+                value={todo.name}
+                onChange={(e) => editTodo(index, e.target.value)}
+            /> :
+            <ListItemText primary={todo.name} />}
+            <IconButton edge="end" onClick={() => setEditIndex(index)} style={{ marginRight: 5 }}>
                 <EditIcon />
-            </IconButton> */}
+            </IconButton>
             <IconButton edge="end" onClick={() => removeTodo(index)} style={{ marginRight: 5 }}>
                 <DeleteIcon />
             </IconButton>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -11,6 +11,8 @@ const TodoList: React.FC = () => {
         color: string
     }[]>([]);
 
+    const [editIndex, setEditIndex] = useState<number | null>(null);
+
     const addTodo = (todo: { name: string, color: string }) => {
         setTodos([...todos, todo]);
     };
@@ -21,13 +23,28 @@ const TodoList: React.FC = () => {
         setTodos(newTodos);
     };
 
+    const editTodo = (index: number, updatedTodo: string) => {
+        const newTodos = todos.map((todo: { name: string, color: string }, idx) => {
+            if(idx === index) return { name: updatedTodo, color: todo.color }
+            else return todo;
+        })
+        setTodos(newTodos);
+    }
+
     return (
         <div>
             <TodoForm addTodo={addTodo} />
             <List style={{ maxHeight: "60vh", paddingRight: 20, overflow: "auto"}}>
                 {todos.map((todo, index) => {
                     return (
-                        <TodoItem todo={todo} index={index} removeTodo={removeTodo} />
+                        <TodoItem 
+                            todo={todo} 
+                            index={index} 
+                            removeTodo={removeTodo} 
+                            editTodo={editTodo} 
+                            editIndex={editIndex}
+                            setEditIndex={setEditIndex}
+                        />
                     )
                 })}
             </List>
