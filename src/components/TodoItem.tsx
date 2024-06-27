0de25aff431ed44074441b3761252cc0876f4bf2
Surabhi Mitra <39255747+Surabhi1997mitra@users.noreply.github.com>
2024-06-27 23:47:05 +0530

    made changes in edit icon

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ListItem, ListItemText, IconButton, TextField } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
+import CheckIcon from '@material-ui/icons/CheckCircle'
 
 interface TodoItemProps {
     todo: {
@@ -19,18 +20,21 @@ const TodoItem: React.FC<TodoItemProps> = ({ todo, index, editTodo, removeTodo, 
     return (
         <ListItem style={{ borderRadius: 10, margin: "1rem 0", border: "1px solid grey", padding: 0 }}>
             <div style={{ width: 30, height: 50, marginRight: 10, borderRadius: "10px 0 0 10px", background: todo.color }}></div>
-            {editIndex === index ?
-            <TextField
-                fullWidth
-                value={todo.name}
-                onChange={(e) => editTodo(index, e.target.value)}
-            /> :
-            <ListItemText primary={todo.name} />}
-            <IconButton edge="end" onClick={() => setEditIndex(index)} style={{ marginRight: 5 }}>
-                <EditIcon />
+            {editIndex === index
+                ? <TextField
+                    fullWidth
+                    value={todo.name}
+                    onChange={(e) => editTodo(index, e.target.value)}
+                />
+                : <ListItemText primary={todo.name} />}
+            <IconButton edge="end" onClick={() => editIndex === index ? setEditIndex(null) : setEditIndex(index)} style={{ marginRight: 5 }}>
+                {editIndex === index ?
+                    <CheckIcon style={{ color: 'green' }}/>
+                    : <EditIcon color='primary'/>
+                }
             </IconButton>
             <IconButton edge="end" onClick={() => removeTodo(index)} style={{ marginRight: 5 }}>
-                <DeleteIcon />
+                <DeleteIcon style={{ color: '#c70000' }}/>
             </IconButton>
         </ListItem>
     )
